2c196ac3c19fb12c76878d9415490d9e79cb7f0a
d-_-b <vcs@ludo.co.za>
2019-11-10 21:52:38 +0200

    shell loop for fpc core examples build for macos

--- build.macos.sh
+++ build.macos.sh
@@ -5,4 +5,7 @@ TMP=./tmp
 RAYLIB=$BIN/libraylib.dylib
 
 fpc -k"$RAYLIB" -Fl$LIBS -Fu$LIBS -FE$BIN -FU$TMP ./src/animation_test.pas
-fpc -k"$RAYLIB" -Fl$LIBS -Fu$LIBS -FE$BIN -FU$TMP ./examples/core/*.pas
+for pasfile in ./examples/core/*.pas
+do
+  fpc -k"$RAYLIB" -Fl$LIBS -Fu$LIBS -FE$BIN -FU$TMP $pasfile
+done
