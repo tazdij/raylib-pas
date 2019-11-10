f35f00d18361e278357b18abe3d4f2cf56572d16
d-_-b usb3 <vcs@ludo.co.za>
2019-11-10 21:33:19 +0200

    shell loop over core/examples/*.pas to execute fpc per example

--- build.linux.sh
+++ build.linux.sh
@@ -5,4 +5,7 @@ TMP=./tmp
 RAYLIB=$BIN
 
 fpc -Fl$RAYLIB -Fl$LIBS -Fu$LIBS -FE$BIN -FU$TMP ./src/animation_test.pas
-fpc -Fl$RAYLIB -Fl$LIBS -Fu$LIBS -FE$BIN -FU$TMP ./examples/core/*.pas
+for pasfile in ./examples/core/*.pas
+do
+  fpc -Fl$RAYLIB -Fl$LIBS -Fu$LIBS -FE$BIN -FU$TMP $pasfile
+done
