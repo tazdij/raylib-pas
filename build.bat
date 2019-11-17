41b6288e2eca5d2e3c72df6b902952539c99f86f
d-_-b <vcs@ludo.co.za>
2019-11-17 20:24:49 +0200

    build.bat --debug --static, TODO fix static section for windows

--- build.bat
+++ build.bat
@@ -1,3 +1,24 @@
-FOR /R ./examples %%F in (*.pas) do (
-  fpc -Fl./libs -Fu./libs -FE./bin -FU./tmp %%F
+SET debug=
+SET static=false
+
+FOR %%a in (%*) DO (
+  SET cmd=--debug
+  CALL SET arg=%%a:%cmd%=%%
+  IF NOT "x%%arg%%"=="x%%a%%" (
+    SET debug=-g -o1
+  )
+  SET cmd=--static
+  CALL SET arg=%%a:%cmd%=%%
+  IF NOT "x%%arg%%"=="x%%a%%" (
+    SET static=true
+  )
+)
+
+FOR /R ./examples %%F IN (*.pas) DO (
+  if "%static%"=="true" (
+    REM fpc -Fl./libs -Fu./libs -FE./bin -FU./tmp %debug% %%F
+  )
+  if "%static%"=="false" (
+    fpc -Fl./libs -Fu./libs -FE./bin -FU./tmp %debug% %%F
+  )  
 )
